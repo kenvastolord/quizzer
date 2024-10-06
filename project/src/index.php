8d9f1e24467e140c22292636ad9f1d434727d579
Kenneth Delgado <kenvastolord@gmail.com>
2024-10-06 10:08:31 -0600

    Display the number of available quiz questions.
    -Implement a query to count the total number of questions.
    -Show the number of questions dynamically.
    -Ensure the count is updated automatically when new questions are
    added.

--- project/src/index.php
+++ project/src/index.php
@@ -1,5 +1,12 @@
 <?php
 require_once './database.php';
+
+$sql = 'SELECT COUNT(*) AS total_questions FROM questions';
+$stmt = $connection->prepare($sql);
+$stmt->execute();
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+$total_questions = $result['total_questions'];
+
 ?>
 
 <!DOCTYPE html>
@@ -23,7 +30,7 @@ require_once './database.php';
         <h2>Test Your PHP Knowledge</h2>
         <p>This is a multiple choice quiz to test your knowledge of PHP</p>
         <ul>
-          <li><strong>Number of Questions: </strong>5</li>
+          <li><strong>Number of Questions: </strong> <?php echo $total_questions ?></li>
           <li><strong>Type: </strong>Multiple Choice</li>
           <li><strong>Estimated Time: </strong>4 Minutes</li>
         </ul>
